c2061adec76f3e0ecd7f1c3eb694a2b79645b43f
Eddort <webisforce@gmail.com>
2021-09-22 15:01:43 +0300

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,18 @@
 module.exports = {
-    siteMetadata: {
-        title: "Quarkly gatsby project",
-        description: "",
-        author: "@Александр Колесников",
-        siteUrl: "https://example.com"
-    },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-transformer-sharp",
-        "gatsby-plugin-sharp",
-        "gatsby-plugin-styled-components",
-        "gatsby-plugin-resolve-src",
-        "gatsby-plugin-meta-redirect",
-        {
-            resolve: "gatsby-plugin-sitemap",
-            options: {
-                output: "/"
-            }
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // "gatsby-plugin-offline",
-    ],
-};
+  siteMetadata: {
+    title: "Quarkly gatsby project",
+    description: "",
+    author: "@Александр Колесников",
+    siteUrl: "https://example.com",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-meta-redirect",
+
+    `gatsby-plugin-netlify`,
+  ],
+}
